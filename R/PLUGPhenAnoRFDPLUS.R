927452180a6386d7f1701f51c9d03a239b3736a7
JoseLastra <jose.lastra@pucv.cl>
2024-10-28 10:47:29 +0100

    tidy up code

--- R/PLUGPhenAnoRFDPLUS.R
+++ R/PLUGPhenAnoRFDPLUS.R
@@ -13,13 +13,6 @@
 #' @import npphen
 #' @importFrom lubridate yday
 #' @examples
-#' \donttest{
-#' #==========================================================================================================
-#' # Reading landsat data and getting scene dates 
-#' ndmibrick <- rast("~/Dropbox/GITHUB/avocado_testing_site/Pinus_test1_Landsat_NDMI_2000-01-01_2022-12-31.tif")#Load in your raster stack with the dates
-#' #Load in the csv file obtained in GEE - beware column with the dates and "date" as header
-#' dates.table <- read.csv('~/Dropbox/GITHUB/avocado_testing_site/Pinus_test1_Date_Landsat_NDMI_2000_2022.csv')
-#' lan.dates <- as.Date(dates.table$date, format='%d-%m-%Y') #change order to '%Y-%m-%d'if you only get NA. Make sure that in this case there is a column name 'dates' in the csv file
 
 #' #===================================================================================================================
 #' #Create the reference curve
@@ -41,7 +34,7 @@
 #'   p2 <-c(p2,pp)
 #'   d1 <-c(d1,lan.dates)
 #' }
-#' 
+#'
 #' PhenKplot(x=p2,d1,h=1, xlab="DOY",ylab="NDMI",rge=c(0,8000))
 
 #' #=========================================================================
@@ -61,7 +54,7 @@
 PLUGPhenAnoRFDPlus <-
   function(x, phen, dates, h, anop, rge) {
     # a.Preparing dataset
-    
+
     if (length(rge) != 2) {
       stop("rge must be a vector of length 2")
     }
@@ -71,35 +64,35 @@ PLUGPhenAnoRFDPlus <-
     if (length(dates) != length(x)) {
       stop("N of dates and files do not match")
     }
-    
+
     # ref.min <- min(refp)
     # ref.max <- max(refp)
     ano.min <- min(anop)
     ano.max <- max(anop)
     ano.len <- ano.max - ano.min + 1
     len2 <- 2 * ano.len
-    
+
     if (ano.min >= ano.max) {
       stop("for anop, lower value > upper value")
     }
-    
+
     if (all(is.na(x))) {
       return(rep(NA, len2))
     }
-    
+
     DOY <- lubridate::yday(dates)
     DOY[which(DOY == 366)] <- 365
     D1 <- cbind(DOY, phen)
     D2 <- cbind(DOY[ano.min:ano.max], x[ano.min:ano.max])
-    
+
     if (length(unique(D1[, 2])) < 10 | (nrow(D1) - sum(is.na(D1))) < (0.1 * length(D1))) {
       return(rep(NA, len2))
     }
-    
+
     if (all(is.na(D2[, 2]))) {
       return(rep(NA, len2))
     }
-    
+
     # b. Kernel calculation using the reference period (D1)
     if (h != 1 && h != 2) {
       stop("Invalid h")
@@ -110,11 +103,13 @@ PLUGPhenAnoRFDPlus <-
         D1[i, 1] <- DOGS[which(DOGS[, 1] == D1[i, 1], arr.ind = TRUE), 2]
       }
     }
-    
+
     Hmat <- ks::Hpi(na.omit(D1))
     Hmat[1, 2] <- Hmat[2, 1]
-    K1 <- ks::kde(na.omit(D1), H = Hmat, xmin = c(1, rge[1]), 
-                  xmax = c(365, rge[2]), gridsize = c(365, 500))
+    K1 <- ks::kde(na.omit(D1),
+      H = Hmat, xmin = c(1, rge[1]),
+      xmax = c(365, rge[2]), gridsize = c(365, 500)
+    )
     K1Con <- K1$estimate
     for (j in 1:365) {
       Kdiv <- sum(K1$estimate[j, ])
@@ -140,13 +135,13 @@ PLUGPhenAnoRFDPlus <-
         MAXY[i] <- NA
       }
     }
-    
+
     # c. Calculating cumulative bivariate density distribution
-    
+
     h2d <- list()
     h2d$x <- seq(1, 365)
     h2d$y <- seq(rge[1], rge[2], len = 500)
-    h2d$density <- K1Con/sum(K1Con)
+    h2d$density <- K1Con / sum(K1Con)
     uniqueVals <- rev(unique(sort(h2d$density)))
     cumRFDs <- cumsum(uniqueVals)
     names(cumRFDs) <- uniqueVals
@@ -154,26 +149,30 @@ PLUGPhenAnoRFDPlus <-
     h2d$cumDensity[] <- cumRFDs[as.character(h2d$density)]
     na.sta <- first.no.NA.DOY - 1
     na.end <- last.no.NA.DOY + 1
-    if(na.sta>=1) {h2d$cumDensity[1:na.sta,] <- NA}
-    if(na.end<=365) {h2d$cumDensity[na.end:365,] <- NA}
-    
+    if (na.sta >= 1) {
+      h2d$cumDensity[1:na.sta, ] <- NA
+    }
+    if (na.end <= 365) {
+      h2d$cumDensity[na.end:365, ] <- NA
+    }
+
     # c. Anomaly calculation (D2)
-    
+
     if (h == 2) {
       for (i in 1:nrow(D2)) {
         D2[i, 1] <- DOGS[which(DOGS[, 1] == D2[i, 1], arr.ind = TRUE), 2]
       }
     }
-    
+
     # d. Calculating anomalies AND their likelihoods based on D2
-    
+
     # d.1 Anomalies
     Anoma <- rep(NA, ano.len)
     for (i in 1:nrow(D2)) {
       Anoma[i] <- as.integer(D2[i, 2] - MAXY[D2[i, 1]])
     }
     Anoma[1:ano.len]
-    
+
     # d.2 Likelihood
     rowAnom <- matrix(NA, nrow = nrow(D2), ncol = 500)
     for (i in 1:nrow(D2)) {
@@ -182,8 +181,7 @@ PLUGPhenAnoRFDPlus <-
     rowAnom2 <- unlist(apply(rowAnom, 1, function(x) {
       if (all(is.na(x))) {
         NA
-      }
-      else {
+      } else {
         which.min(x)
       }
     }))
@@ -215,10 +213,9 @@ PLUGPhenAnoRFDPlus <-
 #' @param datatype Character vector that determines the interpretation of values written to disk. More information: See \code{\link{writeFormats}}
 #' @param rge A vector containing minimum and maximum values of the response variable used in the analysis. We suggest the use of theoretically based limits. For example in the case of MODIS NDVI or EVI, it ranges from 0 to 10,000, so rge =c(0,10000)
 #' @return RasterStack of nlayers([s]stack*2) containing all anomalies, followed by their likelihoods
-
 #' @import npphen
 #' @import terra
-#' @importFrom lubridate yday 
+#' @importFrom lubridate yday
 #' @import RColorBrewer
 #'
 #' @seealso \code{\link{PLUGPhenAnoRFDPLUS}}
@@ -233,7 +230,7 @@ PLUGPhenAnoRFDMapPLUS <-
   function(s, phen, dates, h, anop, nCluster, outname, datatype, rge) {
     ff <- function(x) {
       # a.Preparing dataset
-      
+
       if (length(rge) != 2) {
         stop("rge must be a vector of length 2")
       }
@@ -246,35 +243,35 @@ PLUGPhenAnoRFDMapPLUS <-
       if (length(x) < length(anop)) {
         stop("Inconsistent anop. Argument anop can't be grater than length(x)")
       }
-      
+
       # ref.min <- min(refp)
       # ref.max <- max(refp)
       ano.min <- min(anop)
       ano.max <- max(anop)
       ano.len <- ano.max - ano.min + 1
       len2 <- 2 * ano.len
-      
+
       if (ano.min >= ano.max) {
         stop("for anop, lower value > upper value")
       }
-      
+
       if (all(is.na(x))) {
         return(rep(NA, len2))
       }
-      
+
       DOY <- lubridate::yday(dates)
       DOY[which(DOY == 366)] <- 365
       D1 <- cbind(DOY, phen)
       D2 <- cbind(DOY[ano.min:ano.max], x[ano.min:ano.max])
-      
+
       if (length(unique(D1[, 2])) < 10 | (nrow(D1) - sum(is.na(D1))) < (0.1 * length(D1))) {
         return(rep(NA, len2))
       }
-      
+
       if (all(is.na(D2[, 2]))) {
         return(rep(NA, len2))
       }
-      
+
       # b. Kernel calculation using the reference period (D1)
       if (h != 1 && h != 2) {
         stop("Invalid h")
@@ -285,7 +282,7 @@ PLUGPhenAnoRFDMapPLUS <-
           D1[i, 1] <- DOGS[which(DOGS[, 1] == D1[i, 1], arr.ind = TRUE), 2]
         }
       }
-      
+
       Hmat <- ks::Hpi(na.omit(D1))
       Hmat[1, 2] <- Hmat[2, 1]
       K1 <- ks::kde(na.omit(D1), H = Hmat, xmin = c(1, rge[1]), xmax = c(365, rge[2]), gridsize = c(365, 500))
@@ -294,10 +291,10 @@ PLUGPhenAnoRFDMapPLUS <-
         Kdiv <- sum(K1$estimate[j, ])
         ifelse(Kdiv == 0, K1Con[j, ] <- 0, K1Con[j, ] <- K1$estimate[j, ] / sum(K1$estimate[j, ]))
       }
-      
-      first.no.NA.DOY <- min(D1[,1][which(is.na(D1[,2])==FALSE)])
-      last.no.NA.DOY <- max(D1[,1][which(is.na(D1[,2])==FALSE)])
-      
+
+      first.no.NA.DOY <- min(D1[, 1][which(is.na(D1[, 2]) == FALSE)])
+      last.no.NA.DOY <- max(D1[, 1][which(is.na(D1[, 2]) == FALSE)])
+
       MAXY <- apply(K1Con, 1, max)
       for (i in 1:365) {
         n.select <- which(K1Con[i, ] == MAXY[i], arr.ind = TRUE)
@@ -309,12 +306,16 @@ PLUGPhenAnoRFDMapPLUS <-
           n <- n.select
           MAXY[i] <- median(K1$eval.points[[2]][n])
         }
-        if (i < first.no.NA.DOY) {MAXY[i] <- NA}
-        if (i > last.no.NA.DOY) {MAXY[i] <- NA}
+        if (i < first.no.NA.DOY) {
+          MAXY[i] <- NA
+        }
+        if (i > last.no.NA.DOY) {
+          MAXY[i] <- NA
+        }
       }
-      
+
       # c. Calculating cumulative bivariate density distribution
-      
+
       h2d <- list()
       h2d$x <- seq(1, 365)
       h2d$y <- seq(rge[1], rge[2], len = 500)
@@ -324,26 +325,30 @@ PLUGPhenAnoRFDMapPLUS <-
       names(cumRFDs) <- uniqueVals
       h2d$cumDensity <- matrix(nrow = nrow(h2d$density), ncol = ncol(h2d$density))
       h2d$cumDensity[] <- cumRFDs[as.character(h2d$density)]
-      na.sta <- first.no.NA.DOY-1
-      na.end <- last.no.NA.DOY+1
-      if(na.sta>=1) {h2d$cumDensity[1:na.sta,] <- NA}
-      if(na.end<=365) {h2d$cumDensity[na.end:365,] <- NA}
-      
+      na.sta <- first.no.NA.DOY - 1
+      na.end <- last.no.NA.DOY + 1
+      if (na.sta >= 1) {
+        h2d$cumDensity[1:na.sta, ] <- NA
+      }
+      if (na.end <= 365) {
+        h2d$cumDensity[na.end:365, ] <- NA
+      }
+
       # d. Calculating anomalies AND their RFD based on D2
-      
+
       if (h == 2) {
         for (i in 1:nrow(D2)) {
           D2[i, 1] <- DOGS[which(DOGS[, 1] == D2[i, 1], arr.ind = TRUE), 2]
         }
       }
-      
+
       # d.1 Anomalies
       Anoma <- rep(NA, ano.len)
       for (i in 1:nrow(D2)) {
         Anoma[i] <- as.integer(D2[i, 2] - MAXY[D2[i, 1]])
       }
       Anoma[1:ano.len]
-      
+
       # d.2 RFD
       rowAnom <- matrix(NA, nrow = nrow(D2), ncol = 500)
       for (i in 1:nrow(D2)) {
@@ -366,7 +371,7 @@ PLUGPhenAnoRFDMapPLUS <-
       AnomPLUSRFD <- c(Anoma[1:ano.len], AnomRFDPerc[1:ano.len])
       AnomPLUSRFD[1:len2]
     }
-    
+
     #----------------------------------------------------------------------------------------
     # cluster processing
     app(s, fun = ff, filename = outname, cores = nCluster, overwrite = T, wopt = list(datatype = datatype))
